bcb6774bc2a20fb8c7ca9d737950e0d6ccf322a8
ZaZons <tiago.ogomes.2004@gmail.com>
2022-11-08 17:31:54 +0000

    Gravar e formatacao de menu

--- main.c
+++ main.c
@@ -12,20 +12,27 @@
 int menu ();
 
 int main() {
+    FILE* saveFile;
+
     tipoIp pontosIp[MAX_IP];
-    int opcao,
-    int nPontos = 0, avariasResolvidas = 0;
+    int nPontos = 0;
+    tipoAvaria avarias[MAX_AVARIAS];
+    int nAvarias = 0;
+    tipoIntervencao intervencoes[MAX_INTERVENCOES];
+    int nIntervencoes = 0;
+
+    int avariasResolvidas = 0;
     float percLED = 0;
 
+    int opcao;
     do {
-        tecLED(nLED, nPontos, &percLED); //
-        avariasResolvidas(nAvarias, &avariasResolvidas);
+        //tecLED(nLED, nPontos, &percLED); //
+        //avariasResolvidas(nAvarias, &avariasResolvidas);
 
-
-        opcao = menu();
+        opcao = menu(nPontos, nAvarias);
         switch(opcao) {
             case 1:
-                quantidadePontos = novoPontoIp(pontosIp, quantidadePontos);
+                nPontos = novoPontoIp(pontosIp, nPontos);
                 break;
             case 2:
 
@@ -40,7 +47,21 @@ int main() {
 
                 break;
             case 6:
+                saveFile = fopen("parque.dat", "wb");
 
+                if (saveFile == 0) {
+                    printf("Erro ao abrir o ficheiro\n");
+                } else {
+                    fwrite(pontosIp, sizeof(tipoIp), MAX_IP, saveFile);
+                    fwrite(nPontos, sizeof(int), 1, saveFile);
+                    fwrite(avarias, sizeof(tipoAvaria), MAX_AVARIAS, saveFile);
+                    fwrite(nAvarias, sizeof(int), 1, saveFile);
+                    fwrite(intervencoes, sizeof(tipoIntervencao), MAX_INTERVENCOES, saveFile);
+                    fwrite(nIntervencoes, sizeof(int), 1, saveFile);
+                    fclose(saveFile);
+
+                    printf("\nDados guardados com sucesso.");
+                }
                 break;
         }
 
@@ -50,11 +71,11 @@ int main() {
 }
 
 
-int menu () {
+int menu (int nPontos, int nAvarias) {
     int opcao = 0;
 
     printf("\n\tGestao de Pontos de Iluminacao Publica(IP)");
-    printf("\nTotal de pontos IP: %d\tPontos IP avariados: %d", 0, 0); //add totalpontos e pontos avariados
+    printf("\nTotal de pontos IP: %d\tPontos IP avariados: %d", nPontos, nAvarias); //add totalpontos e pontos avariados
     printf("\nAvarias resolvidas: %d\tTecnologia LED (%%): %.1f", 0, 0.0); // add avarias resolvidas e tec led
     printf("\n   1. Novo ponto IP");
     printf("\n   2. Registo de Avaria");
